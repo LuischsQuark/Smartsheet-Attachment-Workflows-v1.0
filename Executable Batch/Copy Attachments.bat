16ec4fcb2066a270902eaaff5d1c7d85cbdd7d6b
LuischsQuark <luis_chs15@hotmail.com>
2025-03-03 16:04:30 -0600

    File Updates

--- Executable Batch/Copy Attachments.bat
+++ Executable Batch/Copy Attachments.bat
@@ -1,5 +1,5 @@
 @echo off
-cd /d C:\Users\lchavessolis\Documents\SMAR-Attach-wfls
+cd /d C:\Users\lchavessolis\Documents\SMAR-Attach-wfls\Smartsheet-Attachment-Workflos\Attachment-Copy
 set API_ACCESS_KEY=my_secure_api_key
 start /min cmd /k python copyattachments.py
 timeout /t 5
--- Executable Batch/Move Attachments.bat
+++ Executable Batch/Move Attachments.bat
@@ -1,5 +1,5 @@
 @echo off
-cd /d C:\Users\lchavessolis\Documents\SMAR-Attach-wfls
+cd /d C:\Users\lchavessolis\Documents\SMAR-Attach-wfls\Smartsheet-Attachment-Workflos\Attachment-Move
 start /min cmd /k python moveattachments.py
 timeout /t 5
 start http://127.0.0.1:5000
